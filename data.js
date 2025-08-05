ad0d900b964d06ebd777a5f78a8f13bd58698a05
likesh015 <24mcs015@kgcas.com>
2025-08-05 12:06:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const WeatherData={
+    "Delhi" : {
+        temp:39,
+        description:"Sunny and Hot",
+        icon:"https://cdn-icons-png.flaticon.com/512/869/869869.png"
+    },
+    "Mumbai" : {
+        temp:30,
+        description:"Humid and Rainy",
+        icon:"https://cdn-icons-png.flaticon.com/512/414/414974.png"
+    },
+    "Chennai" : {
+        temp:36,
+        description:"Cloudy and Humid",
+        icon:"https://cdn-icons-png.flaticon.com/512/1163/1163624.png"
+    },
+    "Bangalore" : {
+        temp:27,
+        description:"Pleasant and Cloudy",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQijB3wK03pDah5-ed2tOLeaioIsmMj3QHpuw&s"
+    },
+    "Kolkata" : {
+        temp:33,
+        description:"Thunderstroms expected",
+        icon:"https://cdn-icons-png.flaticon.com/512/1146/1146860.png"
+    },
+};
+}
